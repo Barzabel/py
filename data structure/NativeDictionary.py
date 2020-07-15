b6d7c6f01af4bc59918a1d0df755ea182ab0bee0
Andrey <38911974+Barzabel@users.noreply.github.com>
2020-07-15 23:29:09 +0300

    Add files via upload

---
+++ data structure/NativeDictionary.py
@@ -0,0 +1,46 @@
+class NativeDictionary:
+    def __init__(self, sz):
+        self.size = sz
+        self.slots = [None] * self.size
+        self.values = [None] * self.size
+
+    def __Hash(self,value):
+        a = str(value)
+        res = 0
+        for x in range(len(a)):
+            res = (res // 5 + ord(a[x])) * 13 + 7
+        return res % self.size
+
+    def hash_fun(self, key):
+         res =  self.__Hash(key)
+         index = None
+         for x in range(0, self.size, 1):
+             if self.slots[(res + x) % self.size] == None or self.slots[(res + x) % self.size] == key:
+                 index = (res + x) % self.size
+                 break
+         return index
+
+    def is_key(self, key):
+        hash1 = self.__Hash(key)
+
+        for x in range(0, self.size, 1):
+            if self.slots[(hash1 + x) % self.size] == key:
+                return True
+        return False
+
+    def get(self, key):
+        hash1 = self.__Hash(key)
+        for x in range(0, self.size, 1):
+            if self.slots[(hash1 + x) % self.size] == key:
+                return self.values[(hash1 + x) % self.size]
+        return None
+
+    def put(self, key, value):
+        index = self.hash_fun(key)
+        self.slots[index]=key
+        self.values[index]=value
+
+
+
+
+
