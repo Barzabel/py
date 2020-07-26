211b892b07afe34ee7cf4540ef857f5d46945c92
Andrey <38911974+Barzabel@users.noreply.github.com>
2020-07-26 16:40:55 +0300

    Update GenerateBBSTArray.py

--- Trees/GenerateBBSTArray.py
+++ Trees/GenerateBBSTArray.py
@@ -1,8 +1,39 @@
 def GenerateBBSTArray(a):
     a.sort()
-    i =  len(a)//2
-    if len(a)<2:
-        return a
-    else:
-        res = [a[i]]+GenerateBBSTArray(a[0:i])+GenerateBBSTArray(a[i+1:len(a)])
+    res = [None]*len(a)
+    ur = 0
+    l = len(a) - 1
+    while l > 1:
+        ur = ur + 1
+        l = l//2
+
+
+    li = [a]
+    indexlist2 = [0]
+
+    indexlist1 = []
+    li1 = []
+
+    for x in range(ur+1):
+
+        for y in range(len(indexlist2)):
+
+            res[indexlist2[y]]=li[y][len(li[y])//2]
+
+        for y in range(len(indexlist2)):
+            indexlist1.append(indexlist2[y] * 2 + 1)
+            indexlist1.append(indexlist2[y] * 2 + 2)
+
+
+
+
+            li1.append(li[y][:len(li[y])//2])
+            li1.append(li[y][len(li[y])//2+1:])
+
+        indexlist2 = indexlist1
+        li = li1
+
+        indexlist1 = []
+        li1 = []
+
     return res
