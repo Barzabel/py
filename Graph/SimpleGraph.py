a5d595da3d9db66263b2892dd3c54866a76253c5
Andrey <38911974+Barzabel@users.noreply.github.com>
2020-07-28 19:59:49 +0300

    Add files via upload

---
+++ Graph/SimpleGraph.py
@@ -0,0 +1,59 @@
+class Vertex:
+
+    def __init__(self, val):
+        self.Value = val
+
+
+class SimpleGraph:
+
+    def __init__(self, size):
+        self.max_vertex = size
+        self.m_adjacency = [[0] * size for _ in range(size)]
+        self.vertex = [None] * size
+
+    def AddVertex(self, v):
+
+
+        if v < self.max_vertex and self.vertex[v] is None:
+            NewVer = Vertex(v)
+            self.vertex[v]= NewVer
+        # ваш код добавления новой вершины
+        # с значением value
+        # в свободное место массива vertex
+
+
+        # здесь и далее, параметры v -- индекс вершины
+
+    # в списке  vertex
+    def RemoveVertex(self, v):
+        # ваш код удаления вершины со всеми её рёбрами
+        if self.vertex[v] is None:
+            return
+        for x in self.vertex:
+            if x is not None:
+                self.RemoveEdge(v,x.Value)
+        self.vertex[v]= None
+
+        pass
+
+    def IsEdge(self, v1, v2):
+        # True если есть ребро между вершинами v1 и v2
+        if self.vertex[v1] is not None and self.vertex[v1] is not None:
+            if self.m_adjacency[v1][v2] == 1 and self.m_adjacency[v2][v1] == 1:
+                return True
+        return False
+
+    def AddEdge(self, v1, v2):
+        if self.vertex[v1] is not None and self.vertex[v1]is not None:
+            self.m_adjacency[v1][v2] = 1
+            self.m_adjacency[v2][v1] = 1
+        # добавление ребра между вершинами v1 и v2
+
+
+    def RemoveEdge(self, v1, v2):
+        # удаление ребра между вершинами v1 и v2
+        if self.vertex[v1] is not None and self.vertex[v1] is not None:
+            self.m_adjacency[v1][v2] = 0
+            self.m_adjacency[v2][v1] = 0
+
+
