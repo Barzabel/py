7dad0787ce2a87f00f2a80e4e91b8b880754e150
Andrey <38911974+Barzabel@users.noreply.github.com>
2020-12-28 20:22:27 +0300

    Update les1.py

--- sort/les1.py
+++ sort/les1.py
@@ -2,11 +2,13 @@ def SelectionSortStep(array, i):
 	array[array[i:].index(min(array[i:])) + i], array[i] = array[i], min(array[i:])
 
 
-
 def BubbleSortStep(array):
+	res = True
 	for x , y in zip(range(0, len(array[:-1])), range(1,len(array))):
 		if array[x] > array[y]:
 			array[x], array[y] = array[y], array[x]
+			res = False
+	return res
 
 
 
