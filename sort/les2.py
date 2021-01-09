08e0a7add04726897c46009855ece1fa7b5fc9c7
Andrey <38911974+Barzabel@users.noreply.github.com>
2021-01-09 12:31:36 +0300

    Update les2.py

--- sort/les2.py
+++ sort/les2.py
@@ -1,28 +1,10 @@
-
-"""
-def InsertionSortStep(array, step: int, i: int):
-	if (i + step) > (len(array)-1):
-		return array
-	sortarr = array[0:i]
-	val = array.pop(i+step)
-	indexprev = len(sortarr) 
-	for x in sortarr:
-		if val < x:
-			indexprev = sortarr.index(x)
-			break
-	sortarr.insert(indexprev, val)	
-	return sortarr + array[i:]
-"""
 def InsertionSortStep(array: list, step: int, i: int):
-	index = i + step - 1
-	val = array.pop(i)
-	array[index], val = val, array[index]
-	indexprev = len(array[:i])
-	for x in array[:i]:
-		if val < x:
-			indexprev = array[:i].index(x)
-			break
-
-	array.insert(indexprev, val)
+	sortarr = [array[x] for x in range(0,len(array),step) ]
+	for x in range(0,len(sortarr)):
+		for y in range(x):
+			if sortarr[x] < sortarr[y]:
+				sortarr[x],sortarr[y] = sortarr[y],sortarr[x]
+				continue
+	for x in range(0,len(array),step):
+		array[x] = sortarr.pop(0)
 	return array
-	
