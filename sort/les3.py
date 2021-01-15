68ad1775b1353c8998ec81dab16e88f7a33e3ec4
Andrey <38911974+Barzabel@users.noreply.github.com>
2021-01-15 06:54:41 +0300

    Create les3.py

---
+++ sort/les3.py
@@ -0,0 +1,33 @@
+def InsertionSortStep(array: list, step: int, i: int):
+	sortarr = [array[x] for x in range(i,len(array),step) ]
+	for x in range(0,len(sortarr)):
+		for y in range(x):
+			if sortarr[x] < sortarr[y]:
+				sortarr[x],sortarr[y] = sortarr[y],sortarr[x]
+				continue
+	for x in range(i,len(array),step):
+		array[x] = sortarr.pop(0)
+	return array
+
+
+
+
+def KnuthSequence(array_size: int ) -> list:
+	res = []
+	i = 1
+	while i < array_size:
+		res.append(i)
+		i = 3 * i + 1
+		
+	res.reverse()
+	return res
+
+
+def sortshel(arr):
+	i = KnuthSequence(len(arr))
+	res = []
+	for x in i:
+		for n in range(x):
+			res = InsertionSortStep(arr, x, n)
+
+	return res
