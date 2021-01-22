8fc0706ff7bfc6138bde07f5a993790eb7e6ba95
Andrey <38911974+Barzabel@users.noreply.github.com>
2021-01-22 16:32:02 +0300

    Update 4.py

--- sort/4.py
+++ sort/4.py
@@ -2,20 +2,23 @@ def ArrayChunk(array: list) -> int:
 	res = len(array) // 2
 	i1 = 0
 	i2 = len(array) - 1
-
+	r = array[len(array) // 2]
 	while True:
-		i1 = 0
-		i2 = len(array) - 1
-		r = array[ res ]
+		
 		while array[i1] < r:
 			i1 += 1
 
 		while array[i2] > r:
 			i2 -= 1
+
 		if  i1 == i2-1 and array[i1] > array[i2]:
 			array[i1], array[i2] = array[i2], array[i1]
-			continue
-		if (i1 == i2) or (i1 == i2-1 and array[i1] < array[i2]):
+			
+		elif (i1 == i2) or (i1 == i2-1 and array[i1] < array[i2]):
 			return res
-		
-		array[i1], array[i2] = array[i2], array[i1]
+		else:
+			array[i1], array[i2] = array[i2], array[i1]
+			if array[i1] == r or array[i2] == r:
+				i1 = 0
+				i2 = len(array) - 1
+				r = array[len(array) // 2]
