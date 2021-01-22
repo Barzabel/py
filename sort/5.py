04d543cb910500493a8960d764d881a5ec304914
Andrey <38911974+Barzabel@users.noreply.github.com>
2021-01-22 19:00:45 +0300

    Update and rename 4.py to 5.py

--- sort/5.py
+++ sort/5.py
@@ -1,7 +1,10 @@
+
+
 def ArrayChunk(array: list) -> int:
 	res = len(array) // 2
 	i1 = 0
 	i2 = len(array) - 1
+
 	r = array[len(array) // 2]
 	while True:
 		
@@ -28,4 +31,21 @@ def ArrayChunk(array: list) -> int:
 				res = i1
 			if array[i2] == r:
 				res = i2
+			
+		
+
+
+
+
+def QuickSort( array : list, left: int, right: int ) -> None:
+	arr = array[left:right + 1]
+	index = ArrayChunk(arr)
+	array[left:right +1] = arr
+	
+	
+	if len(array[left:left + index+1])>3:
+		QuickSort( array,left, left + index )
+
+	if len(array[left + index:right])>3:	
+		QuickSort( array,left + index,right )
 			
