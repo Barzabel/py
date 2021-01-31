e072700f0e6688b3d70c585a297b57ed21ef1267
Andrey <38911974+Barzabel@users.noreply.github.com>
2021-01-31 21:42:36 +0300

    Update 10.py

--- sort/10.py
+++ sort/10.py
@@ -7,14 +7,14 @@ def MergeSort(array: list) -> list:
 	right = MergeSort(array[:index])
 	res = []
 
-	while len(left)>0 and len(right)>0:
+	while len(left)>0 and len(right) > 0:
 		if left[0] > right[0]:
 			res.append(right.pop(0))
 		else:
 			res.append(left.pop(0))
-	if len(left)>0:
+	if len(left) > 0:
 		res.extend(left)
-	if len(right)>0:
+	if len(right) > 0:
 		res.extend(right)
 
 	return res
